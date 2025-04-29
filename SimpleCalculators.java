99ce0938708392c1a0032a75faab542eece01235
Katleho Letsabo <katlintl.kl@gmail.com>
2025-04-29 13:20:13 +0200

    Add files via upload

---
+++ SimpleCalculators.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simplecalculator;
+
+/**
+ *
+ * @author OEM
+ */
+public class SimpleCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      double num1 = 6;
+      double num2 = 3;
+      String operation = "+"; //Change operation to one of these "+", "-", "*" or "/"
+    
+      double result = 0;
+      
+     switch (operation) {
+
+   case "+":
+      result = add(num1,num2);
+      break;
+   
+   case "-":
+     result = subtract(num1,num2);
+     break;
+   
+   case "*":
+     result = multiply(num1,num2);
+     break;
+   
+   case "/":
+     if (num2 == 0){
+     result = divide(num1,num2);
+     } else {
+       System.out.println("Error cannot divide by 0");
+       return;
+    }
+    break;
+     default:
+       System.out.println("Error: Invalid operator!");
+       return;
+    }
+    System.out.println("Result: " + result);
+   }
+  public static double add(double a, double b) {
+      return a + b;
+  }
+  public static double subtract(double a, double b) {
+      return a - b;
+  }
+  public static double multiply(double a, double b) {
+      return a * b;
+  }
+  public static divide(double a, double b) {
+      return a / b;
+    }
+    
+}
